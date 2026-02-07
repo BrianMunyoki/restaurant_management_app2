78bda93a1c96427b59f05a6e589a8e86282017fc
Brian Munyoki <brianmunyoki168@gmail.com>
2026-02-07 23:40:34 +0300

    creating chef model

--- home/models.py
+++ home/models.py
@@ -11,6 +11,12 @@ validate_at_symbol=RegexValidator(
 class Customer(models.Model):
     customer_name=models.CharField(50)
     customer_id=models.IntegerField()
-    email_address=models.CharField(max_length=50,validators=[validate_at_symbol] 
-    Order_time=models.TimeField())
+    email_address=models.CharField(max_length=50,validators=[validate_at_symbol] )
+    Order_time=models.TimeField()
+
+class Chef(models.Model):
+    chef_name=models.CharField(50)
+    chef_id=models.IntegerField()
+
+
 

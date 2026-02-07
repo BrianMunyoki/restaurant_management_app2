26051eab3e6f4a7fee38a1ee8cc82c6932fb1c94
Brian Munyoki <brianmunyoki168@gmail.com>
2026-02-07 23:38:14 +0300

    creating customer model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#Define the validator
+
+validate_at_symbol=RegexValidator(
+    regex='@',
+    message='Field must contain the @ symbol',
+    code='invalid_at_symbol'
+)
+class Customer(models.Model):
+    customer_name=models.CharField(50)
+    customer_id=models.IntegerField()
+    email_address=models.CharField(max_length=50,validators=[validate_at_symbol] 
+    Order_time=models.TimeField())
+
--- restaurant_management_app/settings.py
+++ restaurant_management_app/settings.py
@@ -77,9 +77,9 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "restaurant_db",        # your database name
-        "USER": "Brian",      # your mysql user
-        "PASSWORD": "12345",
-        "HOST": "127.0.0.1",
+        "USER": "brian",      # your mysql user
+        "PASSWORD": "Munywoki1258!",
+        "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
